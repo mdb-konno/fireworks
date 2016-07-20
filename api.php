3e7ade269170cad755dfb3452bedd6662d73251a
KONNO <k-konno@mediba.jp>
2016-07-20 16:22:03 +0900

    クォーテーションをダブルで統一

--- api.php
+++ api.php
@@ -10,11 +10,11 @@ $imgNum = count(glob($dir . "*.png")); //ディレクトリ内のPNG数を取得
 * 画像保存処理
 ***/
 //canvasデータがPOSTで送信されてきた場合
-if(isset($_POST['canvasImage'])){
-    $canvas = $_POST['canvasImage'];
+if(isset($_POST["canvasImage"])){
+    $canvas = $_POST["canvasImage"];
 
     //ヘッダに「data:image/png;base64,」が付いているので、それは外す
-    $canvas = preg_replace("/data:[^,]+,/i", '', $canvas);
+    $canvas = preg_replace("/data:[^,]+,/i", "", $canvas);
 
     //残りのデータはbase64エンコードされているので、デコードする
     $canvas = base64_decode($canvas);
@@ -24,9 +24,9 @@ if(isset($_POST['canvasImage'])){
 
     //画像として保存（ディレクトリは任意）
     imagesavealpha($image, TRUE); // 透明色の有効
-    imagepng($image , $dir . 'img' . ($imgNum + 1) . '.png');
+    imagepng($image , $dir . "img" . ($imgNum + 1) . ".png");
 
-    echo ($imgNum + 1) . ',' . $dir . 'img' . ($imgNum + 1) . '.png';
+    echo ($imgNum + 1) . "," . $dir . "img" . ($imgNum + 1) . ".png";
 } else if($_SERVER["REQUEST_METHOD"] == "GET"){
     echo $imgNum;
 }
